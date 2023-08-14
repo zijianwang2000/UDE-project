ce99a694f27835e3414faea17222bf173fc0929e
zijianwang2000 <s6ziwang@uni-bonn.de>
2023-08-14 15:36:07 +0200

    Scripts to run on cluster

--- Run on unicorn/SEIR_1.jl
+++ Run on unicorn/SEIR_1.jl
@@ -13,20 +13,7 @@ gr()
 rng = StableRNG(10)
 
 
-##### 2. Define hyperparameter grid #####
-reg_ω_grid = ((nothing, nothing), ("L2 θ", 0.01), ("L2 θ", 0.005), ("L2 θ", 0.001), ("L2 θ", 0.0005), ("L2 θ", 0.0001), 
-              ("Integral", 0.01), ("Integral", 0.005), ("Integral", 0.001), ("Integral", 0.0005), ("Integral", 0.0001))
-γ_init_grid = (0.1, 0.3, 0.6, 0.8)
-adam_epochs_grid = (7500, 7000, 6000, 5000, 4000)
-bfgs_epochs_grid = (22500, )
-experiments = collect(Iterators.product(reg_ω_grid, γ_init_grid, adam_epochs_grid, bfgs_epochs_grid))
-
-# Select hyperparameters via slurm array id
-array_nr = parse(Int, ARGS[1])
-reg_ω, γ_init, adam_epochs, bfgs_epochs = experiments[array_nr]
-
-
-##### 3. Define the SEIR model and sample one data set #####
+##### 2. Define the SEIR model and sample one data set #####
 function seir!(du, u, p, t)
     α, β, γ, N = p
     du[1] = -β*u[1]*u[3]/N
@@ -58,8 +45,14 @@ scatter!(t[begin:5:end], X_noisy', color = [:blue :goldenrod2 :magenta :green],
      label = ["Noisy data" nothing nothing nothing])
 savefig("Data 1.pdf")
 
+# Also, create 1 test data set by varying initial values
+u0_test = [0.980, 0.012, 0.007, 0.001]
+prob_test = ODEProblem(seir!, u0_test, tspan, p_ode)
+X_test = Array(solve(prob_test, Tsit5(), abstol = 1e-12, reltol = 1e-12, saveat = t))
+save("Test data 1.jld", "X_test", X_test)
 
-##### 4. Set up the UDE #####
+
+##### 3. Set up the UDE #####
 # Define activation function
 rbf(x) = exp.(-(x .^ 2))
 
@@ -80,7 +73,7 @@ function ude_dynamics!(du, u, p, t)
 end
 
 
-##### 5. Method to train the UDE using different regularization techniques and initial values #####
+##### 4. Method to train the UDE using different regularization techniques and initial values #####
 function train_ude(reg_ω, γ_init, adam_epochs, bfgs_epochs)
     # Set up the training
     p_init = (nn = θ_init, γ = γ_init)
@@ -92,19 +85,31 @@ function train_ude(reg_ω, γ_init, adam_epochs, bfgs_epochs)
         Array(solve(_prob, Tsit5(), abstol = 1e-6, reltol = 1e-6, saveat = saveat,
                     sensealg = QuadratureAdjoint(autojacvec=ReverseDiffVJP(true))))
     end
-
+   
     # Define the loss function
     function loss(x, p)   # x: optimized variable, p: parametrization of the objective
         if p.reg == nothing
             X̂ = predict(x)
-            mean(abs2, X_noisy .- X̂)   
+            if size(X̂) == size(X_noisy)
+                mean(abs2, X_noisy .- X̂)  
+            else
+                Inf
+            end
         elseif p.reg == "L2 θ"
             X̂ = predict(x)
-            mean(abs2, X_noisy .- X̂) + p.ω * norm(x.nn)
+            if size(X̂) == size(X_noisy)
+                mean(abs2, X_noisy .- X̂) + p.ω * norm(x.nn)
+            else
+                Inf
+            end
         elseif p.reg == "Integral"
             X̂_fine = predict(x, t)
-            X̂ = X̂_fine[:, begin:5:end]
-            mean(abs2, X_noisy .- X̂) + p.ω * norm(Lux.apply(model, X̂_fine[:, begin:end-1], x.nn, st)[1], 1)
+            if size(X̂_fine) == size(X)
+                X̂ = X̂_fine[:, begin:5:end]
+                mean(abs2, X_noisy .- X̂) + p.ω * norm(Lux.apply(model, X̂_fine[:, begin:end-1], x.nn, st)[1], 1)
+            else 
+                Inf
+            end
         end
     end;
     
@@ -131,12 +136,12 @@ function train_ude(reg_ω, γ_init, adam_epochs, bfgs_epochs)
     
     result = (reg = reg, ω = ω, adam_epochs = adam_epochs, bfgs_epochs = bfgs_epochs, losses = losses, γ_init = γ_init, 
               γ_hist = γ_hist, p_trained = p_trained)
-    save("Results 1/result_$(reg)_$(ω)_$(γ_init)_$(adam_epochs)_$(bfgs_epochs).jld2", "result", result)
+    save("Results 1/result_$(reg)_$(ω)_$(γ_init)_$(adam_epochs)_$(bfgs_epochs)_$(array_nr).jld2", "result", result)
     return result
 end 
 
 
-##### 6. Method to visualize the results #####
+##### 5. Method to visualize the results #####
 function nrmse(y_true, y_pred)
     mse = mean(abs2, y_pred .- y_true)
     rmse = sqrt(mse)
@@ -182,15 +187,38 @@ function visualize(result)
               color = [:blue :goldenrod2 :magenta :green], label = ["True" nothing nothing nothing])
     plot!(t, dynamics_nn', color = [:blue :goldenrod2 :magenta :green], linestyle = :dash, 
           label = ["NN(S,E,I,R)" nothing nothing nothing])
-    # Compute and store NRMSE of learned residual dynamics
-    nrmse_score = nrmse(dynamics_true, dynamics_nn)
-    save("Results 1/NRMSE_$(reg)_$(ω)_$(γ_init)_$(adam_epochs)_$(bfgs_epochs).jld", "nrmse_score", nrmse_score)
+    # Compute NRMSE of learned residual dynamics along the true trajectory
+    nrmse_train = nrmse(dynamics_true, dynamics_nn)  
+    # Do the same for the test data
+    dynamics_test = [-β/N * (X_test[1,:] .* X_test[3,:])'; β/N * (X_test[1,:] .* X_test[3,:])' - α * X_test[2,:]';
+                     α * X_test[2,:]'; zeros(length(t))']
+    dynamics_nn_test = Lux.apply(model, X_test, p_trained.nn, st)[1]
+    nrmse_test = nrmse(dynamics_test, dynamics_nn_test)
+    # Save NRMSE scores
+    nrmses = (train = nrmse_train, test = nrmse_test)    
+    save("Results 1/NRMSE_$(reg)_$(ω)_$(γ_init)_$(adam_epochs)_$(bfgs_epochs)_$(array_nr).jld2", "nrmses", nrmses)
 
     # Combined plot  
     plot(p1, p2, p3, p4, p5, size=(1000,1200), layout=(3,2), margin=16Plots.mm,
-         plot_title="Regularization: $(reg), ω: $(ω), NRMSE: $(round(nrmse_score, digits = 4))")
-    savefig("Plots 1/Plot_$(reg)_$(ω)_$(γ_init)_$(adam_epochs)_$(bfgs_epochs).pdf")
+         plot_title="Regularization: $(reg), ω: $(ω), NRMSE: $(round(nrmse_train, digits = 4))")
+    savefig("Plots 1/Plot_$(reg)_$(ω)_$(γ_init)_$(adam_epochs)_$(bfgs_epochs)_$(array_nr).pdf")
 end
+
+
+##### 6. Define hyperparameter grid ##### 
+reg_ω_grid = ((nothing, nothing), ("L2 θ", 10.0), ("L2 θ", 1.0), ("L2 θ", 0.1), ("L2 θ", 0.01), ("L2 θ", 0.005), 
+              ("L2 θ", 0.001), ("L2 θ", 0.0005), ("L2 θ", 0.0001), ("L2 θ", 0.00001), ("Integral", 10.0), ("Integral", 1.0), 
+              ("Integral", 0.1), ("Integral", 0.01), ("Integral", 0.005), ("Integral", 0.001), ("Integral", 0.0005), 
+              ("Integral", 0.0001), ("Integral", 0.00001))
+γ_init_grid = round.(p_ode[3] .* (1.0/5.0, 1.0/2.0, 1.0/1.15, 1.0, 1.15, 2.0, 5.0), digits=2)
+adam_epochs_grid = (7500, 7000, 6000, 5000, 4000)
+bfgs_epochs_grid = (22500, )
+experiments = collect(Iterators.product(reg_ω_grid, γ_init_grid, adam_epochs_grid, bfgs_epochs_grid))
+
+# Select hyperparameters via slurm array id
+array_nr = parse(Int, ARGS[1])
+reg_ω, γ_init, adam_epochs, bfgs_epochs = experiments[array_nr]
+println("Experiment 1: $(reg_ω[1])_$(reg_ω[2])_$(γ_init)_$(adam_epochs)_$(bfgs_epochs)_$(array_nr)")
 
 
 ##### 7. Train using selected hyperparameters #####
